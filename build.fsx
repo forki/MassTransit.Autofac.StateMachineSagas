64831034b1fb785d8d4f6f7c1a6dd73b2bc65650
Alexey Zimarev <alzi@abax.no>
2016-09-20 16:11:02 +0200

    Fixing the build script for myGet

--- build.fsx
+++ build.fsx
@@ -1,40 +1,95 @@
 // include Fake libs
-#r "src/packages/FAKE/tools/FakeLib.dll"
+#I "packages/FAKE/tools/"
+#r "packages/FAKE/tools/FakeLib.dll"
+#r "packages/FAKE/tools/Fake.Deploy.Lib.dll"
 
 open Fake
 open Fake.Paket
 open Fake.FileUtils
 open Fake.Testing.XUnit2
+open Fake.PaketTemplate
+open Fake.AssemblyInfoFile
+open Fake.ProcessHelper
+open Fake.FileHelper
+open Fake.Json
+
+let pathInfo = directoryInfo "."
+let product = environVarOrDefault "productName" pathInfo.Name
+let company = "Alexey Zimarev"
+let copyright = "Copyright © " + System.DateTime.UtcNow.Year.ToString() + " " + company
 
 // Directories
 let rootDir = currentDirectory
 let buildDir  = currentDirectory + "/bin/"
 let testsDir  = currentDirectory + "/tests/"
-let nugetDir = currentDirectory + "/nuget/"
+let nugetDir = currentDirectory + "/nuget"
 let testOutputDir = currentDirectory + "/"
 
-let nugetApiKey = environVar "BAMBOO_nugetApiKey"
-let nugetPushUrl = environVarOrDefault "BAMBOO_nugetPublishUrl"
-let nugetVersion = environVarOrDefault "BAMBOO_GitVersion_NuGetVersion"
+let nugetFeed = environVar "nugetPublishUrl"
+let nugetApiKey = environVar "nugetApiKey"
+
+let gitversion = environVarOrDefault "GitVersion" "gitversion.exe"
 
 // Filesets
-let appReferences  =
+let appReferences =
     !! "src/**/*.csproj"
-      -- "src/**/*.Tests.csproj"
+      ++ "src/**/*.fsproj"
+      -- "src/**/*Tests.csproj"
 
-let testReferences  =
-    !! "src/**/*.Tests.csproj"
+let testReferences =
+    !! "src/**/*Tests.csproj"
+
+type Version = {
+    Major: int
+    Minor: string
+    Patch: string
+    PreReleaseTag: string
+    PreReleaseTagWithDash: string
+    BuildMetaData: string
+    BuildMetaDataPadded: string
+    FullBuildMetaData: string
+    MajorMinorPatch: string
+    SemVer: string
+    LegacySemVer: string
+    LegacySemVerPadded: string
+    AssemblySemVer: string
+    FullSemVer: string
+    InformationalVersion: string
+    BranchName: string
+    Sha: string
+    NuGetVersionV2: string
+    NuGetVersion: string
+    CommitDate: string
+}
+let mutable gitVer = Unchecked.defaultof<Version>
 
 // Targets
 Target "Clean" (fun _ ->
-    CleanDirs [buildDir; nugetDir]
+    CleanDirs [buildDir; testsDir; nugetDir ]
     MSBuildRelease buildDir "Clean" appReferences
         |> Log "Clean-Output: "
 )
 
-Target "BuildTests" (fun _ ->
-    MSBuildRelease testsDir "Build" testReferences
-        |> Log "BuildTests-Output: "
+Target "SetVersion" (fun _ ->
+
+    let result = ExecProcessAndReturnMessages (fun info ->
+        info.FileName <- gitversion
+        info.WorkingDirectory <- "."
+        info.Arguments <- "/output json") (System.TimeSpan.FromMinutes 5.0)
+
+    if result.ExitCode <> 0 then failwithf "'GitVersion.exe' returned with a non-zero exit code"
+
+    let jsonResult = System.String.Concat(result.Messages)
+
+    jsonResult |> deserialize<Version> |> fun ver -> gitVer <- ver
+
+    CreateCSharpAssemblyInfo "./src/SolutionAssemblyInfo.cs"
+        [Attribute.Product product
+         Attribute.Company company
+         Attribute.Copyright copyright
+         Attribute.Version gitVer.AssemblySemVer
+         Attribute.FileVersion (gitVer.MajorMinorPatch + ".0")
+         Attribute.InformationalVersion gitVer.InformationalVersion]
 )
 
 Target "BuildApp" (fun _ ->
@@ -42,13 +97,9 @@ Target "BuildApp" (fun _ ->
         |> Log "BuildApp-Output: "
 )
 
-Target "Push" (fun _ ->
-    Push (fun p ->
-        {p with
-            ApiKey = nugetApiKey
-            PublishUrl = nugetPushUrl(null)
-            WorkingDir = nugetDir
-        })
+Target "BuildTests" (fun _ ->
+    MSBuildRelease testsDir "Build" testReferences
+        |> Log "BuildTests-Output: "
 )
 
 Target "Pack" (fun _ ->
@@ -56,7 +107,16 @@ Target "Pack" (fun _ ->
         {p with
             OutputPath = nugetDir
             WorkingDir = rootDir + "/src"
-            Version = nugetVersion(null)
+            Version = gitVer.NuGetVersionV2
+         })
+)
+
+Target "Push" (fun _ ->
+    Push (fun p ->
+        {p with
+            ApiKey = nugetApiKey
+            PublishUrl = nugetFeed
+            WorkingDir = nugetDir
         })
 )
 
@@ -65,12 +125,13 @@ Target "Test" (fun _ ->
     |> xUnit2 (fun p ->
                 { p with
                     NUnitXmlOutputPath = Some (testOutputDir + "testresults.xml");
-                    ToolPath = @"src/packages/xunit.runner.console/tools/xunit.console.exe"
+                    ToolPath = @"packages/xunit.runner.console/tools/xunit.console.exe"
                 })
 )
 
 // Build order
 "Clean"
+  ==> "SetVersion"
   ==> "BuildApp"
   ==> "BuildTests"
   ==> "Test"
@@ -78,4 +139,4 @@ Target "Test" (fun _ ->
   ==> "Push"
 
 // start build
-RunTargetOrDefault "Test"
+RunTargetOrDefault "Pack"
